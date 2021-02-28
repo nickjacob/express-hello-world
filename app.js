5630b576c67cdb61ba8f6aff4009c098fdeb43ae
Nick Jacob <nickbjacob@gmail.com>
2021-02-28 12:26:12 -0500

    adding redirect

--- app.js
+++ app.js
@@ -2,6 +2,28 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+const isPresent = (str) => str != null && str.length > 0;
+
+function normalizeUrl(url) {
+  return 'https://' + url.trim().toLowerCase().replace(/^https?:\/\//, '');
+}
+
+app.get("/l", (req, res) => {
+  const url = req.query.u;
+  if (url == null || url.length === 0) {
+    return res.redirect("https://herboldmove.org?utm_source=404");
+  }
+
+  const uri = new URL(normalizeUrl(url));
+  if (!isPresent(uri.searchParams.get("utm_source"))) {
+    uri.searchParams.append("utm_source", "redir");
+  }
+
+  return res.redirect(uri.toString())
+});
+
+app.get("*", (_req, res) => {
+  return res.redirect("https://herboldmove.org?utm_source=redir&utm_campaign=nf")
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
